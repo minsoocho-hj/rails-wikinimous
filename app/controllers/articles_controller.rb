e58e5570ac074d36bb5e6f9be115a5973af5f6b1
minsoo cho <khg04170@gmail.com>
2022-01-17 14:50:28 +0000

    wiki challenge completed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+class ArticlesController < ApplicationController
+
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: 'Article was successfully destroyed.'
+  end
+
+  private
+
+  def article_params
+    params.required(:article).permit(:title, :content)
+  end
+
+  def find_params
+    @article = Article.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 100)
+  )
+end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
